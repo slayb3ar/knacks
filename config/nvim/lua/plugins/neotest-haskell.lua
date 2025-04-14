23432f095a9edb914de906982844ffb88cd7c0b7
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2025-04-14 09:27:05 -0400

    bitnomial nvim recomendations

---
+++ config/nvim/lua/plugins/neotest-haskell.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    -- ...,
+    "mrcjkb/neotest-haskell",
+    "nvim-lua/plenary.nvim",
+  },
+}
---
+++ config/nvim/lua/plugins/telescope-manix.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x", -- Recommended
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "mrcjkb/telescope-manix",
+    -- ...
+  },
+}
