fbde785780412a1ee334641ef4d543975637ee22
slayb3ar <42082827+slayb3ar@users.noreply.github.com>
2025-04-16 11:56:05 -0400

    the lot

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,9 @@
 return {
   "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  enabled = true,
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = function(_, opts)
+    opts.sections.lualine_c[4] = { "filename", path = 1 }
+  end,
 }
--- terminal/.zshrc
+++ terminal/.zshrc
@@ -6,6 +6,10 @@ export VISUAL=/usr/bin/vim
 autoload -U compinit
 compinit
 
+# lazygit
+export PATH="$HOME/go/bin:$PATH"
+
+
 ############################## UI
 export TERM=xterm-256color
 export COLORTERM=truecolor
